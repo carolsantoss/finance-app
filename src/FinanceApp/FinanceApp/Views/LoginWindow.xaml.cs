a31462346b88eb02e3a2fd6814d44d6c2066b0fa
Caroline Oliveira Dos Santos <caah.oliveirasantoss@gmail.com>
2026-01-06 19:08:28 -0300

    Ajustes no layout da tela de Login e Main

--- src/FinanceApp/FinanceApp/Views/LoginWindow.xaml.cs
+++ src/FinanceApp/FinanceApp/Views/LoginWindow.xaml.cs
@@ -9,6 +9,7 @@ namespace FinanceApp.Views
     public partial class LoginWindow : Window
     {
         private readonly AppDbContext _context;
+        private bool _isPasswordVisible = false;
 
         public LoginWindow()
         {
@@ -25,30 +26,60 @@ namespace FinanceApp.Views
             );
         }
 
-        private void Login_Click(object sender, RoutedEventArgs e)
+        private void TogglePasswordVisibility_Click(object sender, RoutedEventArgs e)
         {
-            var vm = new LoginViewModel
+            if (_isPasswordVisible)
             {
-                Email = EmailTextBox.Text,
-                Senha = PasswordBox.Password
-            };
-
-            if (vm.Login())
-            {
-                var user = _context.users.FirstOrDefault(u => u.nm_email == vm.Email);
-
-                if (user != null)
-                {
-                    Session.UsuarioLogado = user;
-                }
-
-                var main = new MainWindow();
-                main.Show();
-                this.Close();
+                // Esconder senha
+                PasswordBox.Password = PasswordTextBox.Text;
+                PasswordBox.Visibility = Visibility.Visible;
+                PasswordTextBox.Visibility = Visibility.Collapsed;
+                EyeIcon.Text = "👁️";
+                _isPasswordVisible = false;
             }
             else
             {
-                MessageBox.Show("Email ou senha inválidos");
+                // Mostrar senha
+                PasswordTextBox.Text = PasswordBox.Password;
+                PasswordBox.Visibility = Visibility.Collapsed;
+                PasswordTextBox.Visibility = Visibility.Visible;
+                PasswordTextBox.Focus();
+                PasswordTextBox.CaretIndex = PasswordTextBox.Text.Length;
+                EyeIcon.Text = "👁️‍🗨️";
+                _isPasswordVisible = true;
+            }
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            if (Session.UsuarioLogado == null)
+            {
+                // Garante que pegamos a senha do campo correto
+                string senha = _isPasswordVisible ? PasswordTextBox.Text : PasswordBox.Password;
+
+                var vm = new LoginViewModel
+                {
+                    Email = EmailTextBox.Text,
+                    Senha = senha
+                };
+
+                if (vm.Login())
+                {
+                    var user = _context.users.FirstOrDefault(u => u.nm_email == vm.Email);
+
+                    if (user != null)
+                    {
+                        Session.UsuarioLogado = user;
+                    }
+
+                    var main = new MainWindow();
+                    main.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Email ou senha inválidos");
+                }
             }
         }
 
@@ -58,6 +89,5 @@ namespace FinanceApp.Views
             register.Show();
             this.Close();
         }
-
     }
 }
